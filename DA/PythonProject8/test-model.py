0f00f3ed0bc5b111c5f9f70244676a1e3523ca7a
NeustroevIL <Neustroev88@mail.ru>
2025-11-13 16:14:52 +0500

    fix: забыл пути поменять

--- DA/PythonProject8/test-model.py
+++ DA/PythonProject8/test-model.py
@@ -4,14 +4,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 
 # Загрузка тренировочных данных с целевой переменной
-train_df = pd.read_csv('C:\\Users\\NeustroevIL\\Documents\\проекты\\ML-2025-practice-case-3\\DA\\PythonProject8\\train_01.csv')
+train_df = pd.read_csv('train_01.csv')
 
 # Разделение признаков и целевой переменной
 X_train = train_df.drop(columns=['FloodProbability'])
 y_train = train_df['FloodProbability']
 
 # Загрузка тестовых данных (без столбца FloodProbability)
-test_df = pd.read_csv('C:\\Users\\NeustroevIL\\Documents\\проекты\\ML-2025-practice-case-3\\DA\\PythonProject8\\test.csv')
+test_df = pd.read_csv('test.csv')
 
 # Проверка, что тестовый набор содержит те же признаки
 assert all(X_train.columns == test_df.columns), "Столбцы теста и обучения не совпадают"
